6d9227f04a5623b793c00763ad6f28820fbc9fda
Tena Rozing <tena.rozing@gauss.dev>
2024-07-31 14:03:14 +0200

    Added filtering and search method for employees

--- src/components/homepage/homepage.component.jsx
+++ src/components/homepage/homepage.component.jsx
@@ -1,26 +1,74 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CardList from "../employees/employees.component";
+import SearchBox from "../search-box/search-box.component";
 
 const Home = () => {
   const [employees, setEmployees] = useState([]);
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
+  const [searchField, setSearchField] = useState("");
+  const [selectedPosition, setSelectedPosition] = useState("");
+  const [jobTitles, setJobTitles] = useState([]);
 
   useEffect(() => {
     axios
       .get("/paganini/api/job-interview/employees")
       .then((response) => {
-        setEmployees(response.data.data);
-        console.log(response.data.data);
+        const employeesData = response.data.data;
+        setEmployees(employeesData);
+        setFilteredEmployees(employeesData);
+
+        const uniqueJobTitles = [
+          ...new Set(employeesData.map((employee) => employee.jobTitle)),
+        ];
+        setJobTitles(uniqueJobTitles);
+        console.log(uniqueJobTitles)
       })
       .catch((error) => {
         console.error("Error fetching the employees!", error);
       });
   }, []);
 
+  useEffect(() => {
+    const newFilteredEmployees = employees.filter((employee) => {
+      const matchesSearch =
+        employee.firstName.toLowerCase().includes(searchField) ||
+        employee.lastName.toLowerCase().includes(searchField);
+      const matchesPosition = selectedPosition
+        ? employee.jobTitle === selectedPosition
+        : true;
+      return matchesSearch && matchesPosition;
+    });
+    setFilteredEmployees(newFilteredEmployees);
+  }, [employees, searchField, selectedPosition]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  const onPositionChange = (event) => {
+    const position = event.target.value;
+    setSelectedPosition(position);
+  };
+
   return (
     <div>
       <h1>Employees</h1>
-      <CardList employees={employees} />
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder="search employees"
+        className=""
+      />
+      <select onChange={onPositionChange} value={selectedPosition}>
+        <option value="">All Positions</option>
+        {jobTitles.map((jobTitle, index) => (
+          <option key={index} value={jobTitle}>
+            {jobTitle}
+          </option>
+        ))}
+      </select>
+      <CardList employees={filteredEmployees} />
     </div>
   );
 };
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  // implicitni return jer ga nema
+  <input
+    className={`search-box ${className}`}
+    type="search" // ovaj type omogucava "x" u input fieldu
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+export default SearchBox;
