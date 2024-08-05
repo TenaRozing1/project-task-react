28c79af6cf40772fe4ca29883caef42a395c3969
Tena Rozing <tena.rozing@gauss.dev>
2024-08-05 10:48:14 +0200

    Styled pagination and added back/next arrows

--- src/components/pagination.component/pagination.component.jsx
+++ src/components/pagination.component/pagination.component.jsx
@@ -1,8 +1,14 @@
-// src/components/pagination/pagination.component.jsx
 import React from "react";
+import "./pagination.styles.scss";
 
-const Pagination = ({ employeesPerPage, totalEmployees, paginate, currentPage }) => {
+const Pagination = ({
+  employeesPerPage,
+  totalEmployees,
+  paginate,
+  currentPage,
+}) => {
   const pageNumbers = [];
+  const totalPages = Math.ceil(totalEmployees / employeesPerPage);
 
   for (let i = 1; i <= Math.ceil(totalEmployees / employeesPerPage); i++) {
     pageNumbers.push(i);
@@ -11,13 +17,38 @@ const Pagination = ({ employeesPerPage, totalEmployees, paginate, currentPage })
   return (
     <nav>
       <ul className="pagination">
+        <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+          <button
+            onClick={() => paginate(currentPage - 1)}
+            className="page-link"
+            disabled={currentPage === 1}
+          >
+            &laquo;
+          </button>
+        </li>
         {pageNumbers.map((number) => (
-          <li key={number} className={`page-item ${number === currentPage ? 'active' : ''}`}>
+          <li
+            key={number}
+            className={`page-item ${number === currentPage ? "active" : ""}`}
+          >
             <button onClick={() => paginate(number)} className="page-link">
               {number}
             </button>
           </li>
         ))}
+        <li
+          className={`page-item ${
+            currentPage === totalPages ? "disabled" : ""
+          }`}
+        >
+          <button
+            onClick={() => paginate(currentPage + 1)}
+            className="page-link"
+            disabled={currentPage === totalPages}
+          >
+            &raquo;
+          </button>
+        </li>
       </ul>
     </nav>
   );
