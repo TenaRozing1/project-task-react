8caca235bfba926ea71239e9c1c0a06c723bf323
Tena Rozing <tena.rozing@gauss.dev>
2024-08-05 10:35:43 +0200

    Edited navbar style + added favicon + added logo

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.styles.scss";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <nav>
-      <ul>
+    <nav className="navbar">
+      <Link to="/">
+        <img src="/robot-logo.png" alt="Robot logo" className="logo-image" />
+      </Link>
+      <ul className={`navbar-links ${isMobileMenuOpen ? "open" : ""}`}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -12,6 +22,9 @@ const Navbar = () => {
           <Link to="/add-employee">Add Employee</Link>
         </li>
       </ul>
+      <div className="navbar-hamburger" onClick={toggleMobileMenu}>
+        <div className="hamburger-icon"></div>
+      </div>
     </nav>
   );
 };
